e9a1823b6e9e5c17a31f5e59c1f41bbda6e2cc98
Claire Violet <code@claire.jetzt>
2025-11-15 16:15:28 -0800

    run prettier on script

--- js/script.js
+++ js/script.js
@@ -1,43 +1,44 @@
-$(document).ready( function() {
+$(document).ready(function () {
+  let greeting = "Hallo,";
+  let hr = new Date().getHours();
 
-	let greeting = "Hallo,";
-	let hr = new Date().getHours();
+  if (hr >= 6 && hr < 12) {
+    greeting = "Good Morning,";
+  } else if (hr == 12) {
+    /*Leave Default*/
+  } else if (hr > 12 && hr < 17) {
+    greeting = "Good Afternoon,";
+  } else if (hr >= 17 && hr < 21) {
+    greeting = "Good Evening,";
+  } else if (hr >= 21 || hr < 6) {
+    /*Leave Default*/
+  } else {
+    /*Unknown, Leave Default*/
+  }
+  $("#greeting").html(greeting);
 
-	if(hr >= 6 && hr < 12) {
-		greeting = "Good Morning,";
-	} else if (hr == 12) {
-		/*Leave Default*/
-	} else if (hr > 12 && hr < 17) {
-		greeting = "Good Afternoon,";
-	} else if (hr >= 17 && hr < 21) {
-		greeting = "Good Evening,";
-	} else if (hr >= 21 || hr < 6) {
-		/*Leave Default*/
-	} else {
-		/*Unknown, Leave Default*/
-	}
-	$('#greeting').html(greeting);
+  let icons = [
+    { icon: "CV", id: "cv" },
+    { icon: "☀", id: "sun" },
+    { icon: "♡", id: "heart" },
+    { icon: "⌘", id: "place-of-interest" },
+    { icon: "↪", id: "return" },
+    { icon: "℅", id: "care-of" },
+    { icon: "⏻", id: "power" },
+  ];
+  let isHovering = false;
 
-	let icons = [
-		{ icon: 'CV', id: 'cv' },
-		{ icon: '☀', id: 'sun' },
-		{ icon: '♡', id: 'heart' },
-		{ icon: '⌘', id: 'place-of-interest' },
-		{ icon: '↪', id: 'return' },
-		{ icon: '℅', id: 'care-of' },
-		{ icon: '⏻', id: 'power' }
-	];
-	let isHovering = false;
-	
-	$('#roundel').mouseenter(function() {
-		if (isHovering) return; // prevent multiple rapid fires caused by child element
-		isHovering = true;
-		
-		let rand = Math.floor(Math.random() * icons.length);
-		$('#roundel').html(`<span class="icon-text" id="${icons[rand].id}">${icons[rand].icon}</span>`);
-	});
-	
-	$('#roundel').mouseleave(function() {
-		isHovering = false;
-	});
+  $("#roundel").mouseenter(function () {
+    if (isHovering) return; // prevent multiple rapid fires caused by child element
+    isHovering = true;
+
+    let rand = Math.floor(Math.random() * icons.length);
+    $("#roundel").html(
+      `<span class="icon-text" id="${icons[rand].id}">${icons[rand].icon}</span>`
+    );
+  });
+
+  $("#roundel").mouseleave(function () {
+    isHovering = false;
+  });
 });
